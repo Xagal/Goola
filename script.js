89344c5c0f839acd13c45e6d3ccd6d963fa75146
Philip Ryan <philip.ryan15@yahoo.com>
2019-11-01 06:36:15 -0400

    finished MVP and added a lot of post MVP

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//function to create products for gallery
 let imageArr1 = ['images/products/nice shirt woman.PNG', 'images/products/fjall backpack.PNG', 'images/products/brown purse.PNG', 'images/products/glasses on table.PNG', 'images/products/woven bag.PNG', 'images/products/black purse.PNG', 'images/products/glasses generic.PNG', 'images/products/red bag.PNG']
 let imageArr2 = ['images/products/louis bag.PNG', 'images/products/red bag.PNG', 'images/products/leatherbag.PNG', 'images/products/louis belt.PNG']
 let productDescRow1 = 'Lorem ipsum dolor sit amet'
@@ -6,8 +5,6 @@ let productDescRow2 = 'Lorem ipsum'
 let descArr = []
 descArr.push(productDescRow1)
 descArr.push(productDescRow2)
-
-
 
 const product = function(arr, arrOfImages, galleryNumber, index) {
     let product = document.createElement('div')
@@ -18,11 +15,6 @@ const product = function(arr, arrOfImages, galleryNumber, index) {
     let imageContainer = document.createElement('div')
     imageContainer.setAttribute('class', 'image-container container')
     product.appendChild(imageContainer)
-
-    // <polyline points='0,50 5,40 10,70 15,20 16,25 20,85 25,95 30,10 35,0 40,100 45,90 48,0 56,0 60,90 65,100 70,10 75,0 80,75 85,85 90,20 95,60 100,50 52.5,70' stroke-width=".1" stroke-linecap="butt" stroke-linejoin="miter"></polyline>
-
-    let svg = document.createElement('svg')
-
 
     let imageWrapper = document.createElement('div')
     imageWrapper.setAttribute('class', 'image-wrapper')
@@ -52,8 +44,7 @@ for (let i = 0; i < 4; i++) {
     product(descArr, imageArr2, 2, i)
 }
 
-$(document).on('scroll', function () {
-    $(".goola").css("top", Math.max(100 - .5*window.scrollY, 0) + "px");
+setInterval(() => {
     let banner = document.getElementById('banner')
     let bannerHeight = banner.style.height
     temp = bannerHeight.split('')
@@ -66,27 +57,32 @@ $(document).on('scroll', function () {
     } else {
         document.getElementById("line").style.opacity = '1'
         document.getElementById("line2").style.opacity = '1'
+    }
+}, 10)
 
-    }
-    if(window.scrollY < 250) {
-        let temp = document.querySelector('.logo-container-nav')
-        temp.setAttribute('id', 'not-active')
-        temp = document.querySelector('.logo.container')
-        temp.setAttribute('id', 'logo-visible')
-        temp = document.querySelector('.shop.container')
-        temp.setAttribute('id', 'shop-shiftright')
-
-    }
-    if(window.scrollY >= 250) {
-        let temp = document.querySelector('.logo-container-nav')
-        temp.setAttribute('id', 'active')
-        temp = document.querySelector('.logo.container')
-        temp.setAttribute('id', 'logo-hidden')
-        temp = document.querySelector('.shop.container')
-        temp.setAttribute('id', 'shop-shiftleft')
-    }
+$(document).on('scroll', function () {
+    $(".goola").css("top", Math.max(100 - .5*window.scrollY, 0) + "px");
 })
-
 $(document).on('scroll', function () {
 	$("#banner").css("height", Math.max(250 - 1*window.scrollY, 0) + "px");
 })
+
+setInterval(() => {
+    let temp
+    if (window.pageYOffset > 259) {
+            temp = document.querySelector('.logo-container-nav')
+            temp.setAttribute('id', 'active')
+            temp = document.querySelector('.logo.container')
+            temp.setAttribute('id', 'logo-hidden')
+            temp = document.querySelector('.shop.container')
+            temp.setAttribute('id', 'shop-shiftleft')
+    }
+            temp = document.querySelector('.logo-container-nav')
+    if (window.pageYOffset < 260 && temp.getAttribute('id') != null) {
+            temp.setAttribute('id', 'not-active')
+            temp = document.querySelector('.logo.container')
+            temp.setAttribute('id', 'logo-visible')
+            temp = document.querySelector('.shop.container')
+            temp.setAttribute('id', 'shop-shiftright')
+    }
+}, 1)
